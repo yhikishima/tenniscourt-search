3ce24feba103aa3fd6cc4c93126f528d4b61268f
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-06-27 23:54:00 +0900

    change file

--- tenniscourt-search/app/controllers/court_controller.rb
+++ tenniscourt-search/app/controllers/court_controller.rb
@@ -1,17 +1,6 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
   def index
-    @search_form = SearchForm.new params[:search_form]
-    @posts = Post.scoped
-
-    if @search_form.q.present?
-      @posts = @posts.title_or_body_matches @search_form.q
-    end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
+    @search = Product.search(param[:q])
+    @products = @search.result
   end
 end
